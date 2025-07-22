ab8665a69b033253329c213dbb89283a78a44906
jamesjoel <james.joelj@gmail.com>
2025-07-22 18:33:36 +0530

    javascript day 1

---
+++ javascript/datatype.js
@@ -0,0 +1,25 @@
+/*
+    Data-Types
+    1. Number               all initiger +/- and decimal
+    2. Boolean              true/false
+    3. String               All char
+    
+    
+    4. Array
+    5. Object
+    6. JSON
+    7. undefined
+    8. null/empty
+    9. date
+    10. Symbol
+
+
+*/
+
+let a = "indore";
+let b = 'indore';
+let c = `indore`;
+
+let d = "10.07.01";
+let Name = "sdfg sdfg se5g sdfvb serg sdgb e4";
+
